c924670b925606565a97faa603140a9cf21c2986
katareena <w10160177@gmail.com>
2023-01-17 10:13:43 +0300

    removes unnecessary styles, refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './global-styles/_base.scss';
+import './global-styles/base.scss';
 import App from './components/app/app';
 
 const root = ReactDOM.createRoot(
